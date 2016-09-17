b0e6e4183f85416479cc4312f214aaefacdcc4f0
GabrielePrestifilippo <gabry.501@live.it>
2016-09-17 03:20:19 +0200

    Improved UI

--- www/js/app.js
+++ www/js/app.js
@@ -1,105 +1,112 @@
 /*jslint browser:true */
-/*global $:false, cordova:false, ons:false, Peer:false, getAudio: false */
+/*global $:false, cordova:false, ons:false, Peer:false */
 
+
+/*
+Ready function: Everything should be called after the device is ready, so inside this function.
+*/
 ons.ready(function () {
-	var myNavigator = document.querySelector("#myNavigator");
-	myNavigator
-		.pushPage("tab1.html");
-
+	//Initialize the application
 	var smartHome = new SmartHome();
+});
 
+
+//here we define all the functionalities
+function SmartHome() {
+	var peer;
+	var conn;
+
+
+	//First the click handler
 	$("#connect").click(function () {
-		smartHome.connect();
-
+		connect();
 	});
 
 	$("#create").click(function () {
-		smartHome.create();
+		create();
 	});
-});
 
-var conn;
-
-var myId;
-var peer;
-
-SmartHome.prototype.create = function () {
-	var id = $('#client').find(":selected").val();
-	peer = new Peer(id, {
-		key: 'eff3gmdpo4t8d7vi'
-	});
-	peer.on('open', function (id) {
-		window.console.log('My peer ID is: ' + id);
-		myId = id;
-	});
-	peer.on('call', this.onReceiveCall);
-};
+	//Then all the functions
 
 
-
-SmartHome.prototype.successCallback = function () {
-	window.console.log("success");
-};
-
-SmartHome.prototype.errorCallback = function (e) {
-	window.console.log("error" + e);
-};
-
-SmartHome.prototype.getAudio = function (successCallback, errorCallback) {
-	navigator.webkitGetUserMedia({
-		audio: true,
-		video: false
-	}, this.successCallback, this.errorCallback);
-};
-
-SmartHome.prototype.connect = function () {
-	var id = $('#server').find(":selected").val();
-	conn = peer.connect(id);
-
-	conn.on('open', function () {
-		conn.on('data', function (data) {
-			window.console.log('Received', data);
+	/* ---------------- WEB RTC AUDIO CALL ---------------- */
+	function create() {
+		var self = this;
+		var id = $('#client').find(":selected").val();
+		peer = new Peer(id, {
+			key: 'eff3gmdpo4t8d7vi'
 		});
-		this.call(id);
-	});
-};
+		peer.on('open', function (id) {
+			$("#connect").show();
+			$(".initQuestion").hide();
 
-SmartHome.prototype.call = function (id) {
+			window.console.log('My peer ID is: ' + id);
+		});
+		peer.on('call', onReceiveCall);
+	}
 
-	getAudio(
-		function (MediaStream) {
-			var call = peer.call(id, MediaStream);
-			call.on('stream', this.onReceiveStream);
-		},
-		function (err) {
-			window.alert("error");
+	function getAudio(successCallback, errorCallback) {
+		navigator.webkitGetUserMedia({
+			audio: true,
+			video: false
+		}, successCallback, errorCallback);
+	}
+
+	function successCallback() {
+		window.console.log("success");
+	}
+
+	function errorCallback(e) {
+		window.console.log("error" + e);
+	}
+
+	function connect() {
+		var id = $('#client').find(":selected").val();
+		var self = this;
+		id == "home" ? id = "tutor" : id = "home";
+		conn = peer.connect(id);
+
+		conn.on('open', function () {
+			conn.on('data', function (data) {
+				window.console.log('Received', data);
+			});
+			call(id);
+		});
+	}
+
+	function call(id) {
+		getAudio(
+			function (MediaStream) {
+				var call = peer.call(id, MediaStream);
+				call.on('stream', onReceiveStream);
+			},
+			function (err) {
+				window.alert("error");
+			}
+		);
+	}
+
+	function onReceiveCall(call) {
+		window.alert("incoming call");
+		getAudio(
+			function (MediaStream) {
+				call.answer(MediaStream);
+			},
+			function (err) {
+				window.console.log(err);
+			}
+		);
+		call.on('stream', onReceiveStream);
+	}
+
+	function onReceiveStream(stream) {
+			var audio = document.getElementById('contact-audio');
+			audio.src = window.URL.createObjectURL(stream);
+			audio.onloadedmetadata = function () {
+				window.alert("call started");
+			};
+
 		}
-	);
-	peer.on('call', this.onReceiveCall);
-};
+		/* ---------------- WEB RTC AUDIO CALL ---------------- */
 
-
-
-SmartHome.prototype.onReceiveCall = function (call) {
-	window.alert("incoming call");
-	getAudio(
-		function (MediaStream) {
-			call.answer(MediaStream);
-		},
-		function (err) {
-			window.console.log(err);
-		}
-	);
-	call.on('stream', this.onReceiveStream);
-};
-
-SmartHome.prototype.onReceiveStream = function (stream) {
-	var audio = document.getElementById('contact-audio');
-	audio.src = window.URL.createObjectURL(stream);
-	audio.onloadedmetadata = function () {
-		window.alert("call started");
-	};
-
-};
-
-function SmartHome() {}
+}
